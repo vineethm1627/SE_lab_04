216bde06e0654a5e2d5197e878c49b13239c078a
VINEETH M <vineethm1627@gmail.com>
2020-01-30 11:45:51 +0530

    New Sort

---
+++ src/se_lab_04/NewSort.java
@@ -0,0 +1,49 @@
+package se_lab_04;
+import java.util.Scanner;
+import java.util.Arrays;
+public class NewSort {
+	// Insertion sort recursive.
+	
+	public static void insertionSort(int arr[], int n)
+	{
+		if(n == 1)  // base condition.
+		{
+			return;
+		}
+		
+		insertionSort(arr, n - 1);
+		int last = arr[n - 1];
+		int j = n - 2;
+		
+		while(j >= 0 && arr[j] > last)
+		{
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = last;
+	}
+	
+	public static void printArray(int arr[])
+	{
+		System.out.println(Arrays.toString(arr));
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the size of the array : ");
+		int size = input.nextInt();
+		
+		int arr[] = new int[size]; // creating a dynamic array.
+		
+		System.out.println("\nEnter the elements of the array :");
+		for(int i = 0; i < size; i++)
+		{
+			arr[i] = input.nextInt();
+		}
+		
+		insertionSort(arr, arr.length);
+		printArray(arr);
+	}
+
+}
